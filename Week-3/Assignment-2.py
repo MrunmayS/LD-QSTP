5d0f9e30b6c2b7c88dadc3a0e0d5969a85daefa4
Mrunmay <mrunmayshelar@gmail.com>
2020-05-27 14:02:12 +0530

    4 coins without overlapping

--- Week-3/Assignment-2.py
+++ Week-3/Assignment-2.py
@@ -2,20 +2,20 @@ import numpy as np
 import cv2
 import matplotlib.pyplot as plt
 
-imgpath = 'E:\LD-QSTP\Week-3\img_02.jpg'
+imgpath = 'E:\LD-QSTP\Week-3\img_05.jpg'
 img =  cv2.imread(imgpath)
 img_og = img.copy()
 img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
 img = cv2.GaussianBlur(img, (21,21),cv2.BORDER_DEFAULT)
-circles = cv2.HoughCircles(img, cv2.HOUGH_GRADIENT, 1.2,100, param1 =50, param2=20, minRadius=220, maxRadius=250 )
+circles = cv2.HoughCircles(img, cv2.HOUGH_GRADIENT, 1.2,1000, param1 =50, param2=20, minRadius=220, maxRadius=250 )
 circles_int = np.array(circles, dtype='int')
 print('No. of coins:',circles_int.shape[1])
 #print(circles_int)
 centres = circles_int[:1,:,:-1]
-centres = centres.reshape((2,circles_int.shape[1]))
+centres = centres.reshape((circles_int.shape[1],2))
 centres1 = np.copy(centres)
-#print(centres[0][0])
+#print(centres)
 
 
 for i in range(0,circles_int.shape[1]-1):
@@ -28,6 +28,7 @@ for i in range(0,circles_int.shape[1]-1):
         c = (a + b)/2
         c = c.astype(int)
         #print(c)
+        #print(a,b)
         cv2.line(img_og, tuple(centres[i]),tuple(centres[j]),(0,255,0),10)
         cv2.putText(img_og,str(dist),tuple(c),cv2.FONT_HERSHEY_SIMPLEX, 4, (255,0,0),5)
         #print(dist)
