103b8eaa4792c9541ba6dfe89ea1baf69958da9b
Mrunmay <mrunmayshelar@gmail.com>
2020-05-29 16:28:12 +0530

    Update Assignment-1.py

--- Week-3/Assignment-1.py
+++ Week-3/Assignment-1.py
@@ -3,43 +3,53 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.cluster import KMeans
 
-
-def find_histogram(clt):
+def find_hist(clt):
     numLabels = np.arange(0, len(np.unique(clt.labels_)) + 1)
     (hist, _) = np.histogram(clt.labels_, bins=numLabels)
+
     hist = hist.astype("float")
     hist /= hist.sum()
+    #print(hist)
     return hist
 
-def pie_chart(hist, centroids):
-    pie_plot = pie
-"""
-def plot_colors2(hist, centroids):
-    bar = np.zeros((50, 300, 3), dtype="uint8")
-    startX = 0
+def plot_colors(hist, centroids):
+	bar = np.zeros((50, 300, 3), dtype = "uint8")
+	startX = 0
+	for (percent, color) in zip(hist, centroids):
+		# plot the relative percentage of each cluster
+		endX = startX + (percent * 300)
+		cv2.rectangle(bar, (int(startX), 0), (int(endX), 50),
+			color.astype("uint8").tolist(), -1)
+		startX = endX
+	return bar
 
-    for (percent, color) in zip(hist, centroids):
-        # plot the relative percentage of each cluster
-        endX = startX + (percent * 300)
-        cv2.rectangle(bar, (int(startX), 0), (int(endX), 50),
-                      color.astype("uint8").tolist(), -1)
-        startX = endX
-
-    # return the bar chart
-    return bar
-"""
 img = cv2.imread("E:\LD-QSTP\Week-3\img_01.png")
+h,w,l = img.shape
 img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
-img = img.reshape((img.shape[0] * img.shape[1],3)) #represent as row*column,channel number
-clt = KMeans(n_clusters=5) #cluster number
+img = img.reshape((h*w,l))
+
+clt = KMeans(n_clusters= 5)
 clt.fit(img)
-explode = (0.01, 0.01, 0.01, 0.01) 
+hist = find_hist(clt)
+bar = plot_colors(hist,clt.cluster_centers_)
+img = img.reshape((h,w,l))
+img1 = cv2.cvtColor(img, cv2.COLOR_RGB2LAB)
 
+img1 = img1.reshape((h*w,l))
+labels = clt.fit_predict(img1)
+quant = clt.cluster_centers_.astype("uint8")[labels]
 
-hist = find_histogram(clt)
-bar = plot_colors2(hist, clt.cluster_centers_)
+img = img.reshape((h,w,l))
+quant = quant.reshape((h,w,l))
+img = cv2.cvtColor(img, cv2.COLOR_LAB2RGB)
+quant = cv2.cvtColor(quant, cv2.COLOR_LAB2RGB)
+plt.axis("off")
+plt.subplot(121)
+plt.imshow(quant)
 
+plt.subplot(122)
 plt.axis("off")
 plt.imshow(bar)
 plt.show()
+
