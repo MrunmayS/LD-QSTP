5ed7765a6722ab04e334db8a0a0ccb80e5ed24a9
Mrunmay <mrunmayshelar@gmail.com>
2020-05-29 12:32:42 +0530

    Update Assignment-1.py

--- Week-3/Assignment-1.py
+++ Week-3/Assignment-1.py
@@ -1,35 +1,45 @@
 import cv2
-import matplotlib.pyplot as plt 
-import numpy as np 
+import numpy as np
+import matplotlib.pyplot as plt
 from sklearn.cluster import KMeans
 
-imgpath = "E:\LD-QSTP\Week-3\img_01.png"
-img_bgr = cv2.imread(imgpath)
-img = cv2.cvtColor(img_bgr, cv2.COLOR_BGR2RGB)
 
 def find_histogram(clt):
-    numLabels = np.arange(0,len(np.unique(clt.labels_))+1)
-    hist, _ - np.histogram(clt.labels_, bins = numLabels)
-
+    numLabels = np.arange(0, len(np.unique(clt.labels_)) + 1)
+    (hist, _) = np.histogram(clt.labels_, bins=numLabels)
     hist = hist.astype("float")
     hist /= hist.sum()
-
     return hist
 
-img = img.reshape((img.shape[0]*img.shape[1],3))
-clt = KMeans(n_clusters=5)
+def pie_chart(hist, centroids):
+    pie_plot = pie
+"""
+def plot_colors2(hist, centroids):
+    bar = np.zeros((50, 300, 3), dtype="uint8")
+    startX = 0
+
+    for (percent, color) in zip(hist, centroids):
+        # plot the relative percentage of each cluster
+        endX = startX + (percent * 300)
+        cv2.rectangle(bar, (int(startX), 0), (int(endX), 50),
+                      color.astype("uint8").tolist(), -1)
+        startX = endX
+
+    # return the bar chart
+    return bar
+"""
+img = cv2.imread("E:\LD-QSTP\Week-3\img_01.png")
+img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+img = img.reshape((img.shape[0] * img.shape[1],3)) #represent as row*column,channel number
+clt = KMeans(n_clusters=5) #cluster number
 clt.fit(img)
+explode = (0.01, 0.01, 0.01, 0.01) 
 
-hist1 = find_histogram(clt)
-plt.imshow(clt)
+
+hist = find_histogram(clt)
+bar = plot_colors2(hist, clt.cluster_centers_)
+
+plt.axis("off")
+plt.imshow(bar)
 plt.show()
-
-"""
-def unique_count_app(a):
-    colors, count = np.unique(a.reshape(-1,a.shape[-1]), axis=0, return_counts=True)
-    print(colors[count.argmax()])
-
-unique_count_app(img)
-plt.imshow(img)
-plt.show()
-"""
